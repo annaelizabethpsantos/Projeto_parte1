a3a80554cb13cfdb10bbfafc47391f59f8538502
Anna Elizabeth <61985544+annaelizabethpsantos@users.noreply.github.com>
2021-03-16 22:06:48 -0300

    Update sculptor.cpp

--- sculptor.cpp
+++ sculptor.cpp
@@ -10,6 +10,7 @@ using namespace std;
 const int MAX = 30;
 const int SMAX = 20;
 
+//Construtor da classe
 Sculptor::Sculptor(int _nx, int _ny, int _nz){
 
 int i,j,k;
@@ -24,6 +25,7 @@ for(i=0;i<_nx;i++){
 
 }
 
+//Destrutor da classe
 Sculptor::~Sculptor(){
     int i,j;
     Voxel ***v;
@@ -36,8 +38,8 @@ Sculptor::~Sculptor(){
     delete [] v;
  cout << "Encerrado." << endl;
 }
-// Construtor com argumentos - Recebe as dimensões do Voxel e as coordenadas do mesmo.
 
+// Construtor com argumentos - Recebe as dimensões do Voxel e as coordenadas do mesmo.
 void Sculptor::setColor(float r, float g, float b, float alpha){
     int i,j,k;
     for(i=0;i<nx;i++){
@@ -54,106 +56,165 @@ void Sculptor::setColor(float r, float g, float b, float alpha){
 
 void Sculptor::putVoxel(int x, int y, int z)
 {
-    int i,j,k;
-    for(i=0;i<nx;i++){
-            for(j=0;j<ny;j++){
-                    for(k=0;k<nz;k++){
-                    if(i == x, j == y ; k == z){
-                        v[i][j][k].isOn = true;
-                    }
-            }
-                    }
-    }
+    v[x][y][z].isOn = true;
+    v[x][y][z].r = _r;
+    v[x][y][z].g = _g;
+    v[x][y][z].b = _p;
+    v[x][y][z].a = _alpha;
 }
 
-void Sculptor::cutVoxel(int x, int y, int z)
-{
-    int i,j,k;
-    for(i=0;i<nx;i++){
-            for(j=0;j<ny;j++){
-                    for(k=0;k<nz;k++){
-                    if(i == x, j == y ; k == z){
-                       v[i][j][k].isOn = false;
-                    }
-            }
-                    }
-    }
+void Sculptor::cutVoxel(int x, int y, int z){
+    v[x][y][z].isOn = false;
 }
-
+//Ativa vários vóxels dentro dos intervalos
 void Sculptor::putBox(int x0, int x1, int y0, int y1, int z0, int z1){
-int i,j,k;
+   int x, y, z;
+   for(x=x0; x<=x1; x++){
+      for(y=y0; y<=y1; y++){
+         for(z=z0; z<=z1; z++){
+            v[x][y][z].isOn = true;
+            v[x][y][z].r = _r;
+            v[x][y][z].g = _g;
+            v[x][y][z].b = _p;
+            v[x][y][z].a = _alpha
+         }
+      }
+   }
+   
 }
 
+//Desativa vários vóxels dentro do intervalo
 void Sculptor::cutBox(int x0, int x1, int y0, int y1, int z0, int z1){
-int i,j,k;
+   int x, y, z;
+   for(x=x0; x<=x1; x++){
+      for(y=y0; y<=y1; y++){
+         for(z=z0; z<=z1; z++){
+            v[i][j][k].r = _r;
+            v[i][j][k].g = _g;
+            v[i][j][k].b = _p;
+            v[i][j][k].a = _alpha;
+            v[i][j][k].isOn = false;
+         }
+      }
+   }
 }
 
-void Sculptor::putSphere(int xcenter, int ycenter, int zcenter, int radius)
-{
-int i,j,k;
-for(i=-radius;i<=radius;i++){
-        for(j=-radius;j<=radius;j++){
-                for(k=-radius;k<=radius;k++){
-                    if(pow(xcenter-i,2)+pow(ycenter-j,2)+pow(zcenter-k,2)<pow(radius,2)){
-                        putVoxel(xcenter-i,ycenter-j,zcenter-k);
-                    }
+void Sculptor::putSphere(int xcenter, int ycenter, int zcenter, int radius){
+    for(int i=0; i< xcenter; i++){
+        for(int j=0; j< ycenter; j++){
+            for(int k=0; k< zcenter; k++){
+                //equacao da esfera
+                if (((i-xcenter)*(i-xcenter)) + ((j-ycenter)*(j-ycenter)) + ((k-zcenter)*(k-zcenter)) <= ((radius)*(radius))){
+                    v[i][j][k].isOn = true;
+                    v[i][j][k].r = _r;
+                    v[i][j][k].g = _g;
+                    v[i][j][k].b = _p;
+                    v[i][j][k].a = _alpha;
                 }
-        }
-                }
-}
-
-void Sculptor::cutSphere(int xcenter, int ycenter, int zcenter, int radius)
-{
-int i,j,k;
-for(i=-radius;i<=radius;i++){
-        for(j=-radius;j<=radius;j++){
-                for(k=-radius;k<=radius;k++){
-                    if(pow(xcenter-i,2)+pow(ycenter-j,2)+pow(zcenter-k,2)<pow(radius,2)){
-                        cutVoxel(xcenter-i,ycenter-j,zcenter-k);
-                    }
-                }
-        }
-                }
-}
-
-void Sculptor::putEllipsoid(int xcenter, int ycenter, int zcenter, int rx, int ry, int rz)
-{
-int i,j,k;
-for(i=-rx;i<=rx;i++){
-        for(j=-ry;j<=ry;j++){
-                for(k=-rz;k<=rz;k++){
-                    if((pow(xcenter-i,2)/pow(rx,2))+(pow(ycenter-j,2)/pow(ry,2))+(pow(zcenter-k,2)/pow(rz,2))<1){
-                        putVoxel(xcenter-i,ycenter-j,zcenter-k);
-                    }
-                }
-        }
-                }
-}
-
-void Sculptor::cutEllipsoid(int xcenter, int ycenter, int zcenter, int rx, int ry, int rz)
-{
-    int i,j,k;
-    for(i=-rx;i<=rx;i++){
-            for(j=-ry;j<=ry;j++){
-                    for(k=-rz;k<=rz;k++){
-                        if((pow(xcenter-i,2)/pow(rx,2))+(pow(ycenter-j,2)/pow(ry,2))+(pow(zcenter-k,2)/pow(rz,2))<1){
-                            cutVoxel(xcenter-i,ycenter-j,zcenter-k);
-                        }
-                    }
             }
-                    }
+        }
+    }        
+}
+
+void Sculptor::cutSphere(int xcenter, int ycenter, int zcenter, int radius){
+   for(int i=0; i< xcenter; i++){
+           for(int j=0; j< ycenter; j++){
+               for(int k=0; k< zcenter; k++){
+                   //equacao da esfera
+                   if (((i-xcenter)*(i-xcenter)) + ((j-ycenter)*(j-ycenter)) + ((k-zcenter)*(k-zcenter)) <= ((radius)*(radius))){
+                       v[i][j][k].isOn = true;
+                       v[i][j][k].r = _r;
+                       v[i][j][k].g = _g;
+                       v[i][j][k].b = _p;
+                       v[i][j][k].a = _alpha;
+                   }
+               }
+           }
+       }
+   }
+}
+
+void Sculptor::putEllipsoid(int xcenter, int ycenter, int zcenter, int rx, int ry, int rz){
+   for(int i=0; i< xcenter; i++){
+        for(int j=0; j< ycenter; j++){
+            for(int k=0; k< zcenter; k++){
+                //equacao da esfera
+                if ((((i-xcenter)*(i-xcenter))/((float)((xcenter)*(xcenter)))) + ((((j-ycenter)*(j-ycenter)))/((float)((ycenter)*(ycenter)))) + ((((k-zcenter)*(k-zcenter)))/((float)((zcenter)*(zcenter)))) <=1.0){
+                    v[i][j][k].isOn = true;
+                    v[i][j][k].r = _r;
+                    v[i][j][k].g = _g;
+                    v[i][j][k].b = _p;
+                    v[i][j][k].a = _alpha;
+                }
+            }
+        }
+    }
+}
+
+void Sculptor::cutEllipsoid(int xcenter, int ycenter, int zcenter, int rx, int ry, int rz){
+    for(int i=0; i< xcenter; i++){
+        for(int j=0; j< ycenter; j++){
+            for(int k=0; k< zcenter; k++){
+                //equacao da esfera
+                if ((((i-xcenter)*(i-xcenter))/((float)((xcenter)*(xcenter)))) + ((((j-ycenter)*(j-ycenter)))/((float)((ycenter)*(ycenter)))) + ((((k-zcenter)*(k-zcenter)))/((float)((zcenter)*(zcenter)))) <=1.0){
+                    v[i][j][k].isOn = true;
+                    v[i][j][k].r = _r;
+                    v[i][j][k].g = _g;
+                    v[i][j][k].b = _p;
+                    v[i][j][k].a = _alpha;
+                }
+            }
+        }
+    }
 }
 
 void Sculptor::writeOFF(char *filename)
 {
-    fstream arq;
-
+   int total, index; 
+   ofstream arq;
+   total = 0;
+   arq.open(filename);
+   f << "OFF\n";
+   
+   for(x=0; x<nx; x++){
+      for(y-0; y<=ny; y++){
+         for(z=0; z<=nz; z++){
+            if(v[x][y][z].isOn == true){
+               total++;
+            }
+         }
+      }
+   }
+   f << total * (qnt_vértices) << " " << total * (qtd_faces) << " 0 \n;
     char nome_arq[SMAX],str_off[5]=".OFF";
+   for(x=0; x<nx; x++){
+      for(y-0; y<=ny; y++){
+         for(z=0; z<=nz; z++){
+            if(v[x][y][z].isOn == true){
+               //aqui dentro vamos escrever os índices dos vértices do cubo
+               f << x - lado << " " << y + lado << " " << z - lado << \n" << flush;
+            }
+         }
+      }
+   }
+   
+   for(x=0; x<nx; x++){
+      for(y-0; y<=ny; y++){
+         for(z=0; z<=nz; z++){
+            if(v[x][y][z].isOn == true){
+               //aqui escrevemos os índices das faces, pulando vértices de 8 em 8
+               index = total*8;
+            }
+         }
+      }
+   }
 
     cout << "Insira o nome para o arquivo." << endl;
     cin.getline(nome_arq,SMAX);
     strcat(nome_arq,str_off);
     arq.open(nome_arq);
+   
+   arq.close;
 }
 
 
